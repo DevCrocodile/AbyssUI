b38558133881918684eb869a19912c3e339ecb75
Carlos Avila <carlos2003chas@gmail.com>
2025-03-20 22:31:42 -0600

    Fix Dropdown menu AGAAAAIN

--- lib/Dropdown/Dropdown.tsx
+++ lib/Dropdown/Dropdown.tsx
@@ -9,8 +9,8 @@ export function Dropdown({ children, trigger }: DropdownProps) {
     const [show, setShow] = useState(false)
 
     return (
-        <div className="w-fit relative transition-all" onClick={() => setShow((curr: boolean) => !curr)}>
-            <div>{trigger}</div>
+        <div className="w-fit relative transition-all">
+            <div onClick={() => setShow((curr: boolean) => !curr)}>{trigger}</div>
             <div className={`overflow-hidden transition-all duration-300 ease-in-out ${show ? "max-h-40 opacity-100" : "max-h-0 opacity-0"}`}>
                 {show && <ul {...open} className="p-1 min-w-max relative right-0 mt-1 bg-white border border-slate-500/20 gap-2 rounded-md shadow overflow-hidden">{children}</ul>}
             </div>
